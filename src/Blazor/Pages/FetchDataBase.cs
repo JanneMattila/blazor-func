7ec318c14aee6a38e414a6ad354c07796a2d10c8
Janne Mattila <janne.mattila@hotmail.fi>
2019-12-30 12:17:00 +0200

    Add shared library project

--- src/Blazor/Pages/FetchDataBase.cs
+++ src/Blazor/Pages/FetchDataBase.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Shared;
 
 namespace Blazor.Pages
 {
@@ -14,17 +15,6 @@ namespace Blazor.Pages
         protected override async Task OnInitializedAsync()
         {
             _forecasts = await Http.GetJsonAsync<WeatherForecast[]>("sample-data/weather.json");
-        }
-
-        public class WeatherForecast
-        {
-            public DateTime Date { get; set; }
-
-            public int TemperatureC { get; set; }
-
-            public string Summary { get; set; }
-
-            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
         }
     }
 }
